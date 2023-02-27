8507de03f7f054c1865bbf76fb9423c24ba443e1
chaehunPark <chaehun.dev@gmail.com>
2023-02-27 21:09:32 +0900

    splash 구현/pastel color 추가/폰트 추가

---
+++ app/src/main/java/com/firstproject/fad/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.firstproject.fad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = Runnable{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        handler.postDelayed(runnable, 3000)
+
+    }
+}
